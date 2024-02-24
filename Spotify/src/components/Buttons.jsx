8f80e9db228138ebbe1bffbfa65a07b1278f8d0a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 19:20:01 +0530

    buttons designing

--- Spotify/src/components/Buttons.jsx
+++ Spotify/src/components/Buttons.jsx
@@ -1,7 +1,7 @@
 const Buttons = () => {
     return (
-        <div>
-            <button className='bg-yellow-400 hover:scale-125 transition-all cursor-pointer text-white '>Continue With Web</button>
+        <div className="bg-zinc-900 flex justify-center items-center gap-5 py-10">
+            <button className='bg-yellow-400 px-7 py-5 text-black hover:scale-125 transition-all cursor-pointer font-bold text-lg rounded-full '>Continue With Web</button>
             <button>Download App</button>
         </div>
     )
