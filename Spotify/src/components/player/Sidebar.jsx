6d8ba5e0cac4d99ae9361967f13e18e76fef54d2
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 19:56:07 +0530

    sizes changed

--- Spotify/src/components/player/Sidebar.jsx
+++ Spotify/src/components/player/Sidebar.jsx
@@ -6,10 +6,10 @@ const Sidebar = () => {
     return (
         <div className="w-80 bg-black text-white">
             <div>
-                <ul className="flex flex-col gap-3">
-                    <li className="flex items-center gap-3 text-xl font-semibold tracking-wide "><AiFillHome className="text-3xl" />Home</li>
-                    <li className="flex items-center gap-3 text-xl font-semibold tracking-wide "><LuSearch className="text-3xl" />Search</li>
-                    <li className="flex items-center gap-3 text-xl font-semibold tracking-wide "><MdLibraryBooks className="text-3xl" />Library</li>
+                <ul className="flex flex-col gap-3 pl-3 pt-3">
+                    <li className="flex items-center gap-3 text-lg font-semibold tracking-wide "><AiFillHome className="text-2xl" />Home</li>
+                    <li className="flex items-center gap-3 text-lg font-semibold tracking-wide "><LuSearch className="text-2xl" />Search</li>
+                    <li className="flex items-center gap-3 text-lg font-semibold tracking-wide "><MdLibraryBooks className="text-2xl" />Library</li>
                 </ul>
             </div>
         </div>
