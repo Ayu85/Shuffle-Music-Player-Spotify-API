c73a7fd28f39144bd2e0d0a0d5315158e41d6140
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 20:08:07 +0530

    sidebar making

--- Spotify/src/components/player/Sidebar.jsx
+++ Spotify/src/components/player/Sidebar.jsx
@@ -6,12 +6,12 @@ import { SiMusicbrainz } from "react-icons/si";
 const Sidebar = () => {
     return (
         <div className="w-80 h-full flex flex-col gap-5 bg-black text-white">
-            <div className="flex items-center text-4xl font-[lateral] tracking-wider capitalize gap-3 pl-3 pt-3"><SiMusicbrainz className="text-7xl"/><h1>Shuffle</h1></div>
+            <div className="flex items-center text-4xl font-[lateral] tracking-wider capitalize gap-3 pl-3 pt-3"><SiMusicbrainz className="text-7xl" /><h1>Shuffle</h1></div>
             <div className="border-b border-b-slate-400 pb-4">
-                <ul className="flex flex-col gap-3 pl-3 pt-3">
-                    <li className="flex items-center gap-3 text-lg font-semibold tracking-wide "><AiFillHome className="text-2xl" />Home</li>
-                    <li className="flex items-center gap-3 text-lg font-semibold tracking-wide "><LuSearch className="text-2xl" />Search</li>
-                    <li className="flex items-center gap-3 text-lg font-semibold tracking-wide "><MdLibraryBooks className="text-2xl" />Library</li>
+                <ul className="flex flex-col   pt-3">
+                    <li className="flex hover:bg-slate-600 px-3 cursor-pointer transition-all duration-200 py-1 items-center gap-3 text-lg font-semibold tracking-wide "><AiFillHome className="text-2xl" />Home</li>
+                    <li className="flex hover:bg-slate-600 px-3 cursor-pointer transition-all duration-200 py-1 items-center gap-3 text-lg font-semibold tracking-wide "><LuSearch className="text-2xl" />Search</li>
+                    <li className="flex hover:bg-slate-600 px-3 cursor-pointer transition-all duration-200 py-1 items-center gap-3 text-lg font-semibold tracking-wide "><MdLibraryBooks className="text-2xl" />Library</li>
                 </ul>
             </div>
         </div>
