68d704464fc9011e85231afadce348cbc6160e89
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 21:47:27 +0530

    data retrieved from api

--- Spotify/src/components/player/Player.jsx
+++ Spotify/src/components/player/Player.jsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react"
+import { useSelector } from 'react-redux'
+import axios from 'axios'
 const Player = () => {
+    const token = useSelector(store => store.access_token.value)
+    useEffect(() => {
+        const fetchPlaylists = async () => {
+            const data = await axios.get('https://api.spotify.com/v1/browse/featured-playlists',
+                {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                        "Content-Type": "application/json"
+                    },
+                }
+            );
+            console.log(data);
+        }
+        fetchPlaylists()
+    }, [])
     return (
         <div className="w-full bg-center h-full bg-[url('https://preview.wolfthemes.live/app/uploads/sites/31/2019/11/joseph-young-IOzGybDmKR8-unsplash.jpg')]">
             <div className="w-full h-full backdrop-blur-sm bg-[#00000083]">
