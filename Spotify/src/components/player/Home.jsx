2a67d28f9e121b8a70526e383db1e38cef1dfc09
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 20:16:39 +0530

    making player ui

--- Spotify/src/components/player/Home.jsx
+++ Spotify/src/components/player/Home.jsx
@@ -1,9 +1,11 @@
+import Player from "./Player"
 import Sidebar from "./Sidebar"
 
 const Home = () => {
     return (
-        <div className="h-[100vh]">
+        <div className="h-[100vh] flex">
             <Sidebar/>
+            <Player/>
         </div>
     )
 }
---
+++ Spotify/src/components/player/Player.jsx
@@ -0,0 +1,9 @@
+const Player = () => {
+    return (
+        <div className="w-full bg-center h-full bg-[url('https://preview.wolfthemes.live/app/uploads/sites/31/2019/11/joseph-young-IOzGybDmKR8-unsplash.jpg')]">
+            player
+        </div>
+    )
+}
+
+export default Player
