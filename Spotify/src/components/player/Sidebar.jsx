eafb6f941d445b8f1bb65eab460cd9f023f8e946
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 19:55:16 +0530

    icons added

--- Spotify/src/components/player/Sidebar.jsx
+++ Spotify/src/components/player/Sidebar.jsx
@@ -1,11 +1,15 @@
+import { AiFillHome } from "react-icons/ai";
+import { LuSearch } from "react-icons/lu";
+import { MdLibraryBooks } from "react-icons/md";
+
 const Sidebar = () => {
     return (
         <div className="w-80 bg-black text-white">
             <div>
-                <ul>
-                    <li>Home</li>
-                    <li>Search</li>
-                    <li>Library</li>
+                <ul className="flex flex-col gap-3">
+                    <li className="flex items-center gap-3 text-xl font-semibold tracking-wide "><AiFillHome className="text-3xl" />Home</li>
+                    <li className="flex items-center gap-3 text-xl font-semibold tracking-wide "><LuSearch className="text-3xl" />Search</li>
+                    <li className="flex items-center gap-3 text-xl font-semibold tracking-wide "><MdLibraryBooks className="text-3xl" />Library</li>
                 </ul>
             </div>
         </div>
