ca76a3ab126cb801235e8ff7f3f6b3817102ccbc
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 18:29:43 +0530

    giving skew animation

--- Spotify/src/components/Hero.jsx
+++ Spotify/src/components/Hero.jsx
@@ -10,29 +10,31 @@ const Hero = () => {
     return (
         <div className="relative h-[100vh] flex items-center justify-center overflow-hidden bg-top-left bg-[url('https://shuffle.qodeinteractive.com/wp-content/uploads/2016/09/main-home-slide-1a.jpg')] bg-no-repeat ">
             {/* <img src="https://preview.wolfthemes.live/app/uploads/sites/31/2019/11/neonbrand-MENIBa0eZeA-unsplash.jpg" className="absolute" alt="" /> */}
-            <div className="font-[spotify] flex  absolute w-full h-full  items-center justify-center backdrop-blur-sm bg-[#0000001f]">
-                <motion.img initial={{ translateY: -1000 }} animate={{ translateY: 0 }}
-                    transition={{ duration: 1, ease: easeIn }}
-                    src={s} alt="" className={` w-52 -ml-5 aspect-square`} />
-                <motion.img initial={{ translateY: -1000 }} animate={{ translateY: 0 }}
-                    transition={{ duration: 0.5, ease: easeIn, delay: 0.5 }}
-                    src={h} alt="" className={` w-52 -ml-5 aspect-square`} />
-                <motion.img initial={{ translateY: -1000 }} animate={{ translateY: 0 }}
-                    transition={{ duration: 0.5, ease: easeIn, delay: 0.7 }}
-                    src={u} alt="" className={` w-52 -ml-5 aspect-square`} />
-                <motion.img initial={{ translateY: -1000, delay: 0.5 }} animate={{ translateY: 0 }}
-                    transition={{ duration: 0.5, ease: easeIn, delay: 0.9 }}
-                    src={f1} alt="" className={` w-52 -ml-5 aspect-square`} />
-                <motion.img initial={{ translateY: -1000 }} animate={{ translateY: 0 }}
-                    transition={{ duration: 0.5, ease: easeIn, delay: 1.1 }}
-                    src={f2} alt="" className={` w-52 -ml-5 aspect-square`} />
-                <motion.img initial={{ translateY: -1000 }} animate={{ translateY: 0 }}
-                    transition={{ duration: 0.5, ease: easeIn, delay: 1.3 }}
-                    src={l} alt="" className={` w-52 -ml-5 aspect-square`} />
-                <motion.img initial={{ translateY: -1000 }} animate={{ translateY: 0 }}
-                    transition={{ duration: 0.5, ease: easeIn, delay: 1.5 }}
-                    src={e} alt="" className={` w-52 -ml-5 aspect-square`} />
+            <div className="  flex  items-center justify-center absolute w-full h-full  backdrop-blur-sm bg-[#0000001f]">
+                <div className='flex  items-center justify-center tilter'>
+                    <motion.img initial={{ translateY: -1000 }} animate={{ translateY: 0 }}
+                        transition={{ duration: 1, ease: easeIn }}
+                        src={s} alt="" className={` w-52 -ml-5 aspect-square`} />
+                    <motion.img initial={{ translateY: -1000 }} animate={{ translateY: 0 }}
+                        transition={{ duration: 0.5, ease: easeIn, delay: 0.5 }}
+                        src={h} alt="" className={` w-52 -ml-5 aspect-square`} />
+                    <motion.img initial={{ translateY: -1000 }} animate={{ translateY: 0 }}
+                        transition={{ duration: 0.5, ease: easeIn, delay: 0.7 }}
+                        src={u} alt="" className={` w-52 -ml-5 aspect-square`} />
+                    <motion.img initial={{ translateY: -1000, delay: 0.5 }} animate={{ translateY: 0 }}
+                        transition={{ duration: 0.5, ease: easeIn, delay: 0.9 }}
+                        src={f1} alt="" className={` w-52 -ml-5 aspect-square`} />
+                    <motion.img initial={{ translateY: -1000 }} animate={{ translateY: 0 }}
+                        transition={{ duration: 0.5, ease: easeIn, delay: 1.1 }}
+                        src={f2} alt="" className={` w-52 -ml-5 aspect-square`} />
+                    <motion.img initial={{ translateY: -1000 }} animate={{ translateY: 0 }}
+                        transition={{ duration: 0.5, ease: easeIn, delay: 1.3 }}
+                        src={l} alt="" className={` w-52 -ml-5 aspect-square`} />
+                    <motion.img initial={{ translateY: -1000 }} animate={{ translateY: 0 }}
+                        transition={{ duration: 0.5, ease: easeIn, delay: 1.5 }}
+                        src={e} alt="" className={` w-52 -ml-5 aspect-square`} />
 
+                </div>
             </div>
         </div>
     )
