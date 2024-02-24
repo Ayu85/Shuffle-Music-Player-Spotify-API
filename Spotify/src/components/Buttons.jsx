9ad79983116b119d1d5e92c8928f9a156d2ebf74
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 19:23:22 +0530

    react icons installed

--- Spotify/src/components/Buttons.jsx
+++ Spotify/src/components/Buttons.jsx
@@ -1,7 +1,13 @@
+import { VscDebugContinue } from "react-icons/vsc";
+
 const Buttons = () => {
     return (
         <div className="bg-zinc-900 flex justify-center items-center gap-5 py-10">
-            <button className='bg-yellow-400 px-7 py-5 text-black hover:scale-125 transition-all cursor-pointer font-bold text-lg rounded-full '>Continue With Web</button>
+            <button
+                className='bg-yellow-400 flex items-center gap-2 px-7 py-5 text-black hover:scale-125
+              transition-all cursor-pointer font-bold text-lg rounded-full '>
+                Continue With Web<VscDebugContinue />
+            </button>
             <button>Download App</button>
         </div>
     )
