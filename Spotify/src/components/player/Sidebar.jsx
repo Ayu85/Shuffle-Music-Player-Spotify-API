37274a2dbefe98fb2d9ba6d9ce44cd8ccc250b4f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 19:59:50 +0530

    adding page logo

--- Spotify/src/components/player/Sidebar.jsx
+++ Spotify/src/components/player/Sidebar.jsx
@@ -1,10 +1,12 @@
 import { AiFillHome } from "react-icons/ai";
 import { LuSearch } from "react-icons/lu";
 import { MdLibraryBooks } from "react-icons/md";
+import { SiMusicbrainz } from "react-icons/si";
 
 const Sidebar = () => {
     return (
-        <div className="w-80 h-full bg-black text-white">
+        <div className="w-80 h-full flex flex-col gap-5 bg-black text-white">
+            <div className="flex items-center text-4xl font-[spotify] gap-3 pl-3 pt-3"><SiMusicbrainz className="text-7xl"/><h1>Shuffle</h1></div>
             <div className="border-b border-b-slate-400 pb-4">
                 <ul className="flex flex-col gap-3 pl-3 pt-3">
                     <li className="flex items-center gap-3 text-lg font-semibold tracking-wide "><AiFillHome className="text-2xl" />Home</li>
