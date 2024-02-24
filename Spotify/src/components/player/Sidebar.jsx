2e1c8f94c4ac354f86d07f8e2936daa1bdf5e761
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 20:06:16 +0530

    fonts added

--- Spotify/src/components/player/Sidebar.jsx
+++ Spotify/src/components/player/Sidebar.jsx
@@ -6,7 +6,7 @@ import { SiMusicbrainz } from "react-icons/si";
 const Sidebar = () => {
     return (
         <div className="w-80 h-full flex flex-col gap-5 bg-black text-white">
-            <div className="flex items-center text-4xl font-[spotify] gap-3 pl-3 pt-3"><SiMusicbrainz className="text-7xl"/><h1>Shuffle</h1></div>
+            <div className="flex items-center text-4xl font-[lateral] tracking-wider capitalize gap-3 pl-3 pt-3"><SiMusicbrainz className="text-7xl"/><h1>Shuffle</h1></div>
             <div className="border-b border-b-slate-400 pb-4">
                 <ul className="flex flex-col gap-3 pl-3 pt-3">
                     <li className="flex items-center gap-3 text-lg font-semibold tracking-wide "><AiFillHome className="text-2xl" />Home</li>
