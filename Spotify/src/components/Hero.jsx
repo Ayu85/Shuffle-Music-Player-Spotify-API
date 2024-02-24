064fcd052e19fabb9bc56738571b50f25d69aaa6
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 18:12:03 +0530

    animating heading

--- Spotify/src/components/Hero.jsx
+++ Spotify/src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     return (
         <div className="relative h-[100vh] flex items-center justify-center overflow-hidden bg-top-left bg-[url('https://shuffle.qodeinteractive.com/wp-content/uploads/2016/09/main-home-slide-1a.jpg')] bg-no-repeat ">
             {/* <img src="https://preview.wolfthemes.live/app/uploads/sites/31/2019/11/neonbrand-MENIBa0eZeA-unsplash.jpg" className="absolute" alt="" /> */}
-            <div className="font-[spotify] flex    absolute w-full h-full  items-center justify-center backdrop-blur-sm bg-[#0000001f]">
+            <div className="font-[spotify] flex  absolute w-full h-full  items-center justify-center backdrop-blur-sm bg-[#0000001f]">
                 {[s, h, u, f1, f2, l, e].map((items, key) => {
                     return <span key={key}><img src={items} alt="" className='w-52 -ml-5 aspect-square' /></span>
                 })}
