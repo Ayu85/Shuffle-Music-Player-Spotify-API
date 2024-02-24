b435102fe54a91cb53a48874a608dff0093646cf
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 18:03:28 +0530

    assets for name

--- Spotify/src/components/Hero.jsx
+++ Spotify/src/components/Hero.jsx
@@ -2,7 +2,7 @@ const Hero = () => {
     return (
         <div className="relative h-[100vh] overflow-hidden bg-top-left bg-[url('https://shuffle.qodeinteractive.com/wp-content/uploads/2016/09/main-home-slide-1a.jpg')] bg-no-repeat ">
             {/* <img src="https://preview.wolfthemes.live/app/uploads/sites/31/2019/11/neonbrand-MENIBa0eZeA-unsplash.jpg" className="absolute" alt="" /> */}
-            <div className="font-[spotify] text-[20rem]  uppercase font-extrabold absolute">
+            <div className="font-[spotify] text-[20rem] text-slate-50  uppercase font-extrabold absolute">
                 {['s', 'h', 'u', 'f', 'f', 'l', 'e'].map((items, key) => {
                     return <span key={key}>{items}</span>
                 })}
