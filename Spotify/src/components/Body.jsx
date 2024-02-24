46ac56b7033087c3f5877113a5f077e844a55198
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 14:29:51 +0530

    custom hook accessed

--- Spotify/src/components/Body.jsx
+++ Spotify/src/components/Body.jsx
@@ -2,10 +2,11 @@ import { useSelector } from 'react-redux';
 import Login from './Login'
 
 import Spotify from './Spotify';
+import useToken from '../hooks/useToken';
 
 const Body = () => {
   const token = useSelector(store => store.access_token.value)
-
+  useToken()
   return token ? <Spotify /> : <Login />
 }
 
