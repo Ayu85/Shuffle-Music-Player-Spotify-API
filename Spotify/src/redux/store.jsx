129f2d1b2e9219ead654c72456ed4eb555194a12
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 14:03:34 +0530

    token provided to store

--- Spotify/src/redux/store.jsx
+++ Spotify/src/redux/store.jsx
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import token from './slices/token';
 const store = configureStore({
     reducer: {
-
+        access_token: token
     }
 })
 export default store;
