21861d1624da3b4dfd1443f436fcda636b987402
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 19:27:34 +0530

    icons added

--- Spotify/src/components/Buttons.jsx
+++ Spotify/src/components/Buttons.jsx
@@ -1,14 +1,17 @@
 import { VscDebugContinue } from "react-icons/vsc";
+import { FaGooglePlay } from "react-icons/fa";
 
 const Buttons = () => {
     return (
-        <div className="bg-zinc-900 flex justify-center items-center gap-5 py-10">
+        <div className=" flex justify-center items-center gap-10 py-10 bg-center h-96 bg-[url('https://preview.wolfthemes.live/app/uploads/sites/31/2019/11/neonbrand-MENIBa0eZeA-unsplash.jpg')]">
             <button
-                className='bg-yellow-400 flex items-center gap-2 px-7 py-5 text-black hover:scale-125
+                className='bg-yellow-400 flex items-center gap-2 px-7 py-5 text-black hover:scale-110
               transition-all cursor-pointer font-bold text-lg rounded-full '>
                 Continue With Web<VscDebugContinue />
             </button>
-            <button>Download App</button>
+            <button className='bg-white flex items-center gap-2 px-7 py-5 text-black hover:scale-110
+              transition-all cursor-pointer font-bold text-lg rounded-full '>Download App<FaGooglePlay />
+            </button>
         </div>
     )
 }
