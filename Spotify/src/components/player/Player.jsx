6f6dbf8902cc1700bc023eef1af42cc356680f3b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 20:17:50 +0530

    ui designed

--- Spotify/src/components/player/Player.jsx
+++ Spotify/src/components/player/Player.jsx
@@ -1,7 +1,9 @@
 const Player = () => {
     return (
         <div className="w-full bg-center h-full bg-[url('https://preview.wolfthemes.live/app/uploads/sites/31/2019/11/joseph-young-IOzGybDmKR8-unsplash.jpg')]">
-            player
+            <div className="w-full h-full backdrop-blur-sm bg-[#00000083]">
+                player
+            </div>
         </div>
     )
 }
