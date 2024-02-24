35c8d253a17de3ff68045d406732badae9391c71
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-24 19:35:05 +0530

    adding navigation

--- Spotify/src/components/Buttons.jsx
+++ Spotify/src/components/Buttons.jsx
@@ -3,15 +3,17 @@ import { FaGooglePlay } from "react-icons/fa";
 
 const Buttons = () => {
     return (
-        <div className=" flex justify-center items-center gap-10 py-10 bg-center h-96 bg-[url('https://preview.wolfthemes.live/app/uploads/sites/31/2019/11/neonbrand-MENIBa0eZeA-unsplash.jpg')]">
-            <button
-                className='bg-yellow-400 flex items-center gap-2 px-7 py-5 text-black hover:scale-110
+        <div className=" flex justify-center items-center gap-10 py-10 bg-top-left h-96 bg-[url('https://preview.wolfthemes.live/app/uploads/sites/31/2019/11/neonbrand-MENIBa0eZeA-unsplash.jpg')]">
+            <div className="flex justify-center items-center gap-10 bg-[#00000083] backdrop-blur-sm w-full  h-96">
+                <button
+                    className='bg-yellow-400 flex items-center gap-2 px-7 py-5 text-black hover:scale-110
               transition-all cursor-pointer font-bold text-lg rounded-full '>
-                Continue With Web<VscDebugContinue />
-            </button>
-            <button className='bg-white flex items-center gap-2 px-7 py-5 text-black hover:scale-110
+                    Continue With Web<VscDebugContinue />
+                </button>
+                <button className='bg-white flex items-center gap-2 px-7 py-5 text-black hover:scale-110
               transition-all cursor-pointer font-bold text-lg rounded-full '>Download App<FaGooglePlay />
-            </button>
+                </button>
+            </div>
         </div>
     )
 }
