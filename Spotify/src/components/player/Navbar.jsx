e4030f956122b59374977b1662601f21a046cd06
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 11:58:25 +0530

    custom component provided to player

--- Spotify/src/components/player/Navbar.jsx
+++ Spotify/src/components/player/Navbar.jsx
@@ -1,9 +1,12 @@
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                <input type="text0" placeholder="Search Songs,Artists,Playlists" className="pl-3 w-96 py-4 rounded-full" />
+            </div>
+            <div></div>
+        </div>
+    )
 }
 
 export default Navbar
--- Spotify/src/components/player/Player.jsx
+++ Spotify/src/components/player/Player.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./Navbar"
 
 const Player = () => {
-   
+
     return (
         <div className="w-full bg-center  overflow-y-scroll bg-[url('https://preview.wolfthemes.live/app/uploads/sites/31/2019/11/joseph-young-IOzGybDmKR8-unsplash.jpg')]">
             <div className="w-full h-full backdrop-blur-sm bg-[#00000083]">
-                player
+                <Navbar />
             </div>
         </div>
     )
