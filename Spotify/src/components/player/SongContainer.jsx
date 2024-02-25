17cfed28da107edf1c2756d52a700b189a6a9e5d
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 13:51:01 +0530

    song slice subscribed

--- Spotify/src/components/player/SongContainer.jsx
+++ Spotify/src/components/player/SongContainer.jsx
@@ -1,10 +1,13 @@
+import { useSelector } from "react-redux"
+
 const SongContainer = () => {
-    
-  return (
-    <div>
-      
-    </div>
-  )
+    const songs = useSelector(store => store.songs.value)
+    return (
+        < div >
+
+        </div >
+    )
 }
+
 
 export default SongContainer
