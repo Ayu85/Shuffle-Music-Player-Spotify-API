c8e5f8e36ac2c88575467516b3d97c93c7ed0575
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 18:40:00 +0530

    custom hook created

--- Spotify/src/hooks/useCurrentTrack.jsx
+++ Spotify/src/hooks/useCurrentTrack.jsx
@@ -1,6 +1,21 @@
-
+import { useEffect } from "react"
+import { useSelector } from 'react-redux'
+import axios from 'axios'
 const useCurrentTrack = () => {
-
+    const token = useSelector(store => store.access_token.value)
+    useEffect(() => {
+        const getTrack = async () => {
+            const track = await axios.get('https://api.spotify.com/v1/me/player/currently-playing',
+                {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                        "Content-Type": "application/json"
+                    }
+                })
+            console.log(track);
+        }
+        getTrack()
+    }, [])
 }
 
 export default useCurrentTrack
