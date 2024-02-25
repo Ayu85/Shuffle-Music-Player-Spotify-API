cded8e422b7fc629da48a6ec5b0fb638dee7cef3
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 19:08:54 +0530

    adding player controls

--- Spotify/src/components/player/CurrentTrack.jsx
+++ Spotify/src/components/player/CurrentTrack.jsx
@@ -2,14 +2,16 @@ import { useSelector } from 'react-redux'
 const CurrentTrack = () => {
     const track = useSelector(store => store.current_track.value)
     return (
-        <div className='bg-[#000000] flex text-white  sticky h-24 gap-3 items-center pl-3 bottom-0 backdrop-blur-lg'>
-            <div>
-                <img src={track?.item?.album?.images[0]?.url} alt="" className='w-20'/>
-            </div>
-            <div>
-                <h1 className='font-semibold'>{track?.item?.name}</h1>
-                <h1 className='text-sm text-slate-400'>{track?.item?.album?.artists[0].name},{track?.item?.album?.artists[1]?.name}</h1>
-                <h1></h1>
+        <div className='bg-[#000000] flex text-white  sticky h-24  items-center pl-3 bottom-0 backdrop-blur-lg'>
+            <div className='flex gap-3'>
+                <div>
+                    <img src={track?.item?.album?.images[0]?.url} alt="" className='w-20' />
+                </div>
+                <div>
+                    <h1 className='font-semibold'>{track?.item?.name}</h1>
+                    <h1 className='text-sm text-slate-400'>{track?.item?.album?.artists[0].name},{track?.item?.album?.artists[1]?.name}</h1>
+                    <h1></h1>
+                </div>
             </div>
         </div>
     )
