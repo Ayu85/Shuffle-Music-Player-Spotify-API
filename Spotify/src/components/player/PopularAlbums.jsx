01e151bdde441003bb4df388e8b1b53eede7b499
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 13:06:12 +0530

    displaying albums on ui

--- Spotify/src/components/player/PopularAlbums.jsx
+++ Spotify/src/components/player/PopularAlbums.jsx
@@ -9,11 +9,11 @@ const PopularAlbums = () => {
   )
 }
 const AlbumCard = ({ images, label, name, release_date }) => {
-  return <div className='w-44 aspect-square bg-[#00000089] flex flex-col justify-center text-white'>
-    <img src={images[1]?.url} alt="logo" />
-    <h1>{name}</h1>
-    <h1>{label}</h1>
-    <h1>{release_date}</h1>
+  return <div className='w-44 aspect-square bg-[#00000026] px-3 py-2  flex flex-col justify-center text-white'>
+    <img src={images[1]?.url} alt="logo" className='h-32 ' />
+    <h1 className='text-md font-[joy] line-clamp-1'>{name}</h1>
+    <h1 className='text-sm'>{label}</h1>
+    <h1 className='text-sm'>{release_date}</h1>
   </div>
 }
 export default PopularAlbums
