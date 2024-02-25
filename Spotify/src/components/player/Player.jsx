7aa4696488a69d159d378397fc9dc4d9f32af394
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 18:41:17 +0530

    current track fetched

--- Spotify/src/components/player/Player.jsx
+++ Spotify/src/components/player/Player.jsx
@@ -1,3 +1,4 @@
+import useCurrentTrack from "../../hooks/useCurrentTrack"
 import useSongs from "../../hooks/useSongs"
 import Navbar from "./Navbar"
 import PopularAlbums from "./PopularAlbums"
@@ -5,6 +6,7 @@ import SongContainer from "./SongContainer"
 
 const Player = () => {
     useSongs()
+    useCurrentTrack()
     return (
         <div className="w-full bg-center h-[100vh]  overflow-y-scroll bg-[url('https://preview.wolfthemes.live/app/uploads/sites/31/2019/11/joseph-young-IOzGybDmKR8-unsplash.jpg')]">
             <div className="w-full h-max backdrop-blur-sm bg-[#00000083]">
