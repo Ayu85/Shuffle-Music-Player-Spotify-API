8fbdb36dbf174fd6bd8ac4bb4a9f0a1a06a41f3c
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 11:50:09 +0530

    creating custom hook to fetch playlists

--- Spotify/src/components/player/Player.jsx
+++ Spotify/src/components/player/Player.jsx
@@ -1,25 +1,6 @@
-import { useEffect } from "react"
-import { useDispatch, useSelector } from 'react-redux'
-import axios from 'axios'
-import { addPlaylist } from "../../redux/slices/playlists"
+
 const Player = () => {
-    const token = useSelector(store => store.access_token.value)
-    const dispatch = useDispatch()
-    useEffect(() => {
-        const fetchPlaylists = async () => {
-            const data = await axios.get('https://api.spotify.com/v1/browse/featured-playlists',
-                {
-                    headers: {
-                        Authorization: "Bearer " + token,
-                        "Content-Type": "application/json"
-                    },
-                }
-            );
-            console.log(data);
-            dispatch(addPlaylist(data?.data))
-        }
-        fetchPlaylists()
-    }, [])
+   
     return (
         <div className="w-full bg-center  overflow-y-scroll bg-[url('https://preview.wolfthemes.live/app/uploads/sites/31/2019/11/joseph-young-IOzGybDmKR8-unsplash.jpg')]">
             <div className="w-full h-full backdrop-blur-sm bg-[#00000083]">
---
+++ Spotify/src/hooks/usePlaylist.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios'
+import { addPlaylist } from "../../redux/slices/playlists"
+const usePlaylist = () => {
+    const token = useSelector(store => store.access_token.value)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        const fetchPlaylists = async () => {
+            const data = await axios.get('https://api.spotify.com/v1/browse/featured-playlists',
+                {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                        "Content-Type": "application/json"
+                    },
+                }
+            );
+            console.log(data);
+            dispatch(addPlaylist(data?.data))
+        }
+        fetchPlaylists()
+    }, [])
+}
+
+export default usePlaylist
