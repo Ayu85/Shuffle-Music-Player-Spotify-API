065a20e65b4032e6dffe00475dec30d172a8aa09
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 19:03:17 +0530

    styling of current track

--- Spotify/src/components/player/CurrentTrack.jsx
+++ Spotify/src/components/player/CurrentTrack.jsx
@@ -2,9 +2,11 @@ import { useSelector } from 'react-redux'
 const CurrentTrack = () => {
     const track = useSelector(store => store.current_track.value)
     return (
-        <div className='bg-[#000000da] text-white sticky h-20 bottom-0 backdrop-blur-lg'>
+        <div className='bg-[#000000da] text-white  sticky h-20 bottom-0 backdrop-blur-lg'>
             <div>
-                {track?.item?.name}
+                <h1 className='font-semibold'>{track?.item?.name}</h1>
+                <h1 className='text-sm text-slate-400'>{track?.item?.album?.artists[0].name},{track?.item?.album?.artists[1]?.name}</h1>
+                <h1></h1>
             </div>
             <div></div>
         </div>
