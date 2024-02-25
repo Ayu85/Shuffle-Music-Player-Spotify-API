3097d0225e8c752a0d7424ae2272268c31633c3e
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 14:28:08 +0530

    song details fetched

--- Spotify/src/components/player/SongContainer.jsx
+++ Spotify/src/components/player/SongContainer.jsx
@@ -19,7 +19,7 @@ const Card = ({ track }) => {
     return <div className="flex justify-between px-10 ">
         <div className="text-white flex gap-3">
             <img src={track?.album?.images[2].url} className="w-14" alt="logo" />
-            <div className="flex flex-col">
+            <div className="flex flex-col w-96">
                 <h1 className="font-semibold" >{track?.name}</h1>
                 <h1 className="text-slate-300">{track?.artists[1]?.name}</h1>
             </div>
