0109c0492581b06ac759ad135177be91b6a99d6c
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 14:13:03 +0530

    images fetched

--- Spotify/src/components/player/SongContainer.jsx
+++ Spotify/src/components/player/SongContainer.jsx
@@ -1,16 +1,25 @@
+/* eslint-disable react/prop-types */
 import { useSelector } from "react-redux"
 
 const SongContainer = () => {
     const songs = useSelector(store => store.songs.value)
     return (
-        < div className="" >
+        <div>
             <h1>Songs</h1>
-            {songs.map((song, key) => {
-                return <div key={key}>{song.track.name}</div>
-            })}
-        </div >
+            <div className="flex flex-col gap-3" >
+                {songs.map((song, key) => {
+                    return <Card {...song} key={key} />
+                })}
+            </div >
+        </div>
     )
 }
 
-
+const Card = ({ track}) => {
+    return <div className="flex">
+        <img src={track?.album?.images[2].url} className="w-14" alt="logo" />
+        <h1 className="text-white">{track?.name}</h1>
+        <h1></h1>
+    </div>
+}
 export default SongContainer
