5e3c277495285bddf7078827ae087e0ba1a7162e
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 22:13:40 +0530

    implemented logic but premium required fr next and previous

--- Spotify/src/components/player/CurrentTrack.jsx
+++ Spotify/src/components/player/CurrentTrack.jsx
@@ -1,17 +1,37 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { FaCirclePlay } from "react-icons/fa6";
 import { IoPlaySkipForward } from "react-icons/io5";
 import { IoPlaySkipBack } from "react-icons/io5";
 import { TfiControlShuffle } from "react-icons/tfi";
 import { RiRepeat2Line } from "react-icons/ri";
-
+import axios from 'axios'
+// import useCurrentTrack from '../../hooks/useCurrentTrack';
+import { addTrack } from '../../redux/slices/currentTrack';
 const CurrentTrack = () => {
     const track = useSelector(store => store.current_track.value)
+    const token = useSelector(store => store.access_token.value)
+    const dispatch = useDispatch()
+    const nextSong = async () => {
+        await axios.post('https://api.spotify.com/v1/me/player/next', {},{
+            headers: {
+                Authorization: "Bearer " + token,
+                "Content-Type": "application/json"
+            }
+        });
+        const track = await axios.get('https://api.spotify.com/v1/me/player/currently-playing',
+            {
+                headers: {
+                    Authorization: "Bearer " + token,
+                    "Content-Type": "application/json"
+                }
+            })
+        dispatch(addTrack(track.data))
+    }
     return (
-        <div className='bg-[#000000] flex justify-between px-20 text-white  sticky h-24  items-center bottom-0 backdrop-blur-lg'>
+        <div className='bg-[#000000] flex justify-between px-20 text-white  sticky h-20  items-center bottom-0 backdrop-blur-lg'>
             <div className='flex gap-3'>
                 <div>
-                    <img src={track?.item?.album?.images[0]?.url} alt="" className='w-20' />
+                    <img src={track?.item?.album?.images[0]?.url} alt="" className='w-16' />
                 </div>
                 <div>
                     <h1 className='font-semibold'>{track?.item?.name}</h1>
@@ -20,11 +40,11 @@ const CurrentTrack = () => {
                 </div>
             </div>
             <div className='flex text-4xl gap-4 items-center'>
-                <TfiControlShuffle className='text-xl text-slate-500 cursor-pointer'/>
-                <IoPlaySkipBack className='text-2xl cursor-pointer'/>
+                <TfiControlShuffle className='text-xl text-slate-500 cursor-pointer' />
+                <IoPlaySkipBack className='text-2xl cursor-pointer' />
                 <FaCirclePlay />
-                <IoPlaySkipForward className='text-2xl cursor-pointer' />
-                <RiRepeat2Line className='text-xl text-slate-500 cursor-pointer'/>
+                <IoPlaySkipForward className='text-2xl cursor-pointer' onClick={nextSong} />
+                <RiRepeat2Line className='text-xl text-slate-500 cursor-pointer' />
 
             </div>
         </div>
