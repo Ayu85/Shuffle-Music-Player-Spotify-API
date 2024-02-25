66d7e9929e2991a6f003ec4d5813be7c738baede
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 13:47:53 +0530

    song slice provided to store

--- Spotify/src/redux/store.jsx
+++ Spotify/src/redux/store.jsx
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import token from './slices/token';
 import playlists from './slices/playlists';
 import popular_albums from './slices/popular_albums';
+import songs from './slices/songs';
 const store = configureStore({
     reducer: {
         access_token: token,
         playlist: playlists,
-        albums: popular_albums
+        albums: popular_albums,
+        songs:songs
     }
 })
 export default store;
