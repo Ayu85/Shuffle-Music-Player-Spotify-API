c553884a250b194873b6ba036466fcc133f1090a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 12:17:23 +0530

    styling navbar

--- Spotify/src/components/player/Navbar.jsx
+++ Spotify/src/components/player/Navbar.jsx
@@ -1,10 +1,16 @@
+import { BsFillSearchHeartFill } from "react-icons/bs";
+
 const Navbar = () => {
     return (
-        <div>
-            <div>
-                <input type="text0" placeholder="Search Songs,Artists,Playlists" className="pl-3 w-96 py-4 rounded-full" />
+        <div className="flex justify-between  h-20 items-center ">
+            <div className="relative pl-2">
+                <input type="text0" placeholder="Search Songs,Artists,Playlists"
+                    className="pl-3 outline-none  w-96 py-3 rounded-full " />
+                <BsFillSearchHeartFill className="absolute text-zinc-700 cursor-pointer text-2xl top-3 right-2" />
             </div>
-            <div></div>
+            <div className="bg-black text-white">
+                Aayush
+            </div>
         </div>
     )
 }
--- Spotify/src/components/player/Sidebar.jsx
+++ Spotify/src/components/player/Sidebar.jsx
@@ -9,30 +9,32 @@ const Sidebar = () => {
     const playlist = useSelector(store => store.playlist.value)
     console.log("pl", playlist);
     return (
-        <div className="w-80 h-[100vh]  flex flex-col gap-5 bg-black text-white overflow-y-scroll">
-            <div className="flex items-center text-4xl font-[lateral] tracking-wider capitalize gap-3 pl-3 pt-3"><SiMusicbrainz className="text-7xl" /><h1>Shuffle</h1></div>
-            <div className="border-b border-b-slate-400 pb-4">
-                <ul className="flex flex-col   pt-3">
-                    <li className="flex hover:bg-slate-600 px-3 cursor-pointer transition-all duration-200 py-1 items-center gap-3 text-md font-semibold tracking-wide "><AiFillHome className="text-2xl" />Home</li>
-                    <li className="flex hover:bg-slate-600 px-3 cursor-pointer transition-all duration-200 py-1 items-center gap-3 text-md font-semibold tracking-wide "><LuSearch className="text-2xl" />Search</li>
-                    <li className="flex hover:bg-slate-600 px-3 cursor-pointer transition-all duration-200 py-1 items-center gap-3 text-md font-semibold tracking-wide "><MdLibraryBooks className="text-2xl" />Library</li>
-                </ul>
-            </div>
-            {/* playlists */}
-            <div className="border-b border-b-slate-400 pb-4 ">
-                <h1 className="text-xl flex items-center gap-2 font-semibold tracking-wide pl-3"><RiPlayList2Fill />
-                    {playlist?.message}</h1>
-                <ul className="flex flex-col text-white  pt-3">
+        <div className=" w-80 h-[100vh] overflow-y-scroll  bg-top-left bg-[url('https://preview.wolfthemes.live/app/uploads/sites/31/2019/11/joseph-young-IOzGybDmKR8-unsplash.jpg')] ">
+            <div className=" flex flex-col gap-5  text-white bg-[#00000083] backdrop-blur-md  ">
+                <div className="flex items-center text-4xl font-[lateral] tracking-wider capitalize gap-3 pl-3 pt-3"><SiMusicbrainz className="text-7xl" /><h1>Shuffle</h1></div>
+                <div className="border-b border-b-slate-400 pb-4">
+                    <ul className="flex flex-col   pt-3">
+                        <li className="flex hover:bg-slate-600 px-3 cursor-pointer transition-all duration-200 py-1 items-center gap-3 text-md font-semibold tracking-wide "><AiFillHome className="text-2xl" />Home</li>
+                        <li className="flex hover:bg-slate-600 px-3 cursor-pointer transition-all duration-200 py-1 items-center gap-3 text-md font-semibold tracking-wide "><LuSearch className="text-2xl" />Search</li>
+                        <li className="flex hover:bg-slate-600 px-3 cursor-pointer transition-all duration-200 py-1 items-center gap-3 text-md font-semibold tracking-wide "><MdLibraryBooks className="text-2xl" />Library</li>
+                    </ul>
+                </div>
+                {/* playlists */}
+                <div className="border-b border-b-slate-400 pb-4 ">
+                    <h1 className="text-xl flex items-center gap-2 font-semibold tracking-wide pl-3"><RiPlayList2Fill />
+                        {playlist?.message}</h1>
+                    <ul className="flex flex-col text-white  pt-3">
 
-                    {
-                        playlist?.playlists?.items?.map((list, key) => {
-                            return <li key={key} className="flex hover:bg-slate-600 px-3
+                        {
+                            playlist?.playlists?.items?.map((list, key) => {
+                                return <li key={key} className="flex hover:bg-slate-600 px-3
                              cursor-pointer transition-all duration-200 py-1 items-center 
                              gap-3 text-md font-semibold tracking-wide "><img src={list?.images[0]?.url} className="w-10" alt="" />{list?.name}</li>
-                        })
-                    }
+                            })
+                        }
 
-                </ul>
+                    </ul>
+                </div>
             </div>
         </div>
     )
