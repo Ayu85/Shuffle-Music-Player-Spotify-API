3bd156155747c3aa18c77567e8bb1f04feecf827
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 14:37:02 +0530

    redux store utilised

--- Spotify/src/hooks/usePlaylist.jsx
+++ Spotify/src/hooks/usePlaylist.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
 import { addPlaylist } from "../redux/slices/playlists"
+import { addCurrentPlaylist } from "../redux/slices/currentPlaylist"
 const usePlaylist = () => {
     const token = useSelector(store => store.access_token.value)
     const dispatch = useDispatch()
@@ -17,6 +18,7 @@ const usePlaylist = () => {
             );
             //  console.log(data);
             dispatch(addPlaylist(data?.data))
+            dispatch(addCurrentPlaylist(data?.data?.playlists?.items[0]))
         }
         fetchPlaylists()
     }, [])
